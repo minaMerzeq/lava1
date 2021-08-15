47f7e03a3068447bf11ea342641ddf584138e731
Mina Merzeq <minamerzekluffy@gmail.com>
2021-08-15 02:41:00 +0200

    deploy

---
+++ precache-manifest.df0f94e0be702d3a6aac0d1e0ba57495.js
@@ -0,0 +1,150 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c6deab10b669399706545295b0aa428",
+    "url": "/CompanyProfile.pdf"
+  },
+  {
+    "revision": "f753fea6c6080b7b8745",
+    "url": "/css/about.69d7004c.css"
+  },
+  {
+    "revision": "a7f4debfb309c9fa3a31",
+    "url": "/css/app.da475588.css"
+  },
+  {
+    "revision": "13cca013cd3070d2ed52",
+    "url": "/css/chunk-vendors.ff650d32.css"
+  },
+  {
+    "revision": "006f938e869fea2ad7bd27bcbc8676aa",
+    "url": "/img/1.006f938e.png"
+  },
+  {
+    "revision": "93fdf5fa0738925cd12323dd226bc4f7",
+    "url": "/img/2.93fdf5fa.png"
+  },
+  {
+    "revision": "aba676f8adf06f9512e7f318d61acdb7",
+    "url": "/img/5.aba676f8.png"
+  },
+  {
+    "revision": "31380640bd1397ec191b8601eafc8057",
+    "url": "/img/Burger-Fuel-new.31380640.png"
+  },
+  {
+    "revision": "3fa2669f9a0d5c172b48cd15b64769c4",
+    "url": "/img/ERPNext.3fa2669f.png"
+  },
+  {
+    "revision": "470ca0da8342c974610469fe328745ad",
+    "url": "/img/LavaDo.470ca0da.png"
+  },
+  {
+    "revision": "f0f4e97779d40e267c6bdc246c47de23",
+    "url": "/img/LavaHub-Diagram.f0f4e977.png"
+  },
+  {
+    "revision": "7e429e82c98be42b6a876388fdd4c04c",
+    "url": "/img/Microsoft-Silver.7e429e82.jpg"
+  },
+  {
+    "revision": "b4ec7cc481b1fb2fd0386de9383e616a",
+    "url": "/img/Microsoft-Silver.b4ec7cc4.png"
+  },
+  {
+    "revision": "fe6c4e763b0a7020fc575ecd7d5e2439",
+    "url": "/img/erpnext-service.fe6c4e76.png"
+  },
+  {
+    "revision": "64bcb8a307f40c68218a256d89433d0b",
+    "url": "/img/fb.64bcb8a3.svg"
+  },
+  {
+    "revision": "3c6e4e3363696d5f148ea68ab1478380",
+    "url": "/img/haggag.3c6e4e33.jpg"
+  },
+  {
+    "revision": "4d98dfc09dacfaaf01087602301ac67c",
+    "url": "/img/how-do-we-do-it.4d98dfc0.jpg"
+  },
+  {
+    "revision": "91bd6fece275fc13975f3672308d6dae",
+    "url": "/img/khaled.91bd6fec.jpg"
+  },
+  {
+    "revision": "0ab392c79adbcf3f07e6f843acef5f09",
+    "url": "/img/lavaHub.0ab392c7.png"
+  },
+  {
+    "revision": "7aea605b884d72663d3b01897e89e6d0",
+    "url": "/img/linkedin.7aea605b.svg"
+  },
+  {
+    "revision": "3b8505a3b235790c93429cf2662d4fec",
+    "url": "/img/logo-dark.3b8505a3.png"
+  },
+  {
+    "revision": "e62b8f91c35828650e094d3214f47603",
+    "url": "/img/mask-group.e62b8f91.png"
+  },
+  {
+    "revision": "1f3a54e8bf9de8d941102173c884248f",
+    "url": "/img/ozo-new.1f3a54e8.png"
+  },
+  {
+    "revision": "d51193b3c541253edba91bbc74486cba",
+    "url": "/img/shalaby.d51193b3.jpg"
+  },
+  {
+    "revision": "60c44e78035888494a0dd7b1de7c79e7",
+    "url": "/img/sliderimage.60c44e78.jpg"
+  },
+  {
+    "revision": "5f8de4080e075f635f9824cf4d44ef1c",
+    "url": "/img/tax.5f8de408.png"
+  },
+  {
+    "revision": "a916ac044b5d7090cf9ea57294f6954b",
+    "url": "/img/test5.a916ac04.png"
+  },
+  {
+    "revision": "51d3840f52a09fd0793b1fb4dca4a07a",
+    "url": "/img/test7.51d3840f.png"
+  },
+  {
+    "revision": "49ead405bfaf4e8bd34786d18f564399",
+    "url": "/img/test8.49ead405.png"
+  },
+  {
+    "revision": "1f74fd2adcfab9756cde060cbda67354",
+    "url": "/img/whoweare.1f74fd2a.png"
+  },
+  {
+    "revision": "a7ead3d28a0c962fc2cee0814723f8c2",
+    "url": "/img/wisa-new.a7ead3d2.png"
+  },
+  {
+    "revision": "e00227130df5122e033fba4c50705382",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f753fea6c6080b7b8745",
+    "url": "/js/about.0e21753b.js"
+  },
+  {
+    "revision": "a7f4debfb309c9fa3a31",
+    "url": "/js/app.da77e445.js"
+  },
+  {
+    "revision": "13cca013cd3070d2ed52",
+    "url": "/js/chunk-vendors.0ae97301.js"
+  },
+  {
+    "revision": "f66f88379b9a136e433457b5acc34555",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
